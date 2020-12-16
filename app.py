b4549577f75a463a0c088cc7525cec35b64be413
sevrino <sevrino@naver.com>
2020-12-16 19:48:45 +0900

    타이머 갈아엎는중..

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import logger as l
-import tweet as twt
+import tweet
 import json
 
 with open('./config/setting.json') as json_file:
     json_data = json.load(json_file)
-
     ver = json_data["ver"]
 
 logger = l.log()
 print("프로그램을 실행시켜 주셔서 감사합니다. 프로그램의 버전은 %s입니다. 트위터를 확인해 주세요." % (ver))
-twt = twt.twt()
+start = tweet.twt()
--- tweet.py
+++ tweet.py
@@ -6,11 +6,9 @@ import threading as th
 if __name__ == "__main__":
     print("Error")
 
-
-
 def twitting(message):
     # API Key Load
-    with open('./config/setting.json') as json_file:
+    with open('./config/setting.json', encoding='UTF-8') as json_file:
         json_data = json.load(json_file)
 
         apikey = json_data["api_key"]
@@ -18,22 +16,39 @@ def twitting(message):
         access_token = json_data["access_token"]
         access_token_secret = json_data["access_token_secret"]
         
-    # API Key Load from json files
+    # UPLOAD Function
     auth = tweepy.OAuthHandler(apikey, apisecret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
     api.update_status(message)
 
+
 def twt():
     # Tweet draw
-    number = random.randint(1, 2)
+    number = random.randint(1, 9)
 
     # Load Tweet Data from json file
-    with open('./config/tweet/tweetting.json') as json_file:
+    with open('./config/tweet/tweetting.json', encoding='UTF-8') as json_file:
         json_data = json.load(json_file)
-
         tweet = json_data["%d" % (number)]
 
     # START
     th.Timer(600.0, twt).start()
     twitting(tweet)
+
+"""
+def twt():
+    # Tweet draw
+    number = random.randint(1, 9)
+
+    # Load Tweet Data from json file
+    with open('./config/tweet/tweetting.json', encoding='UTF-8') as json_file:
+        json_data = json.load(json_file)
+
+        tweet = json_data["%d" % (number)]
+
+    with open('./config/setting.json', encoding='UTF-8') as timer:
+        timer_json = json.load(timer)
+        timer_v = timer_json["Timer"]
+        timer = timer_json["%d" % (timer_v)]
+"""
